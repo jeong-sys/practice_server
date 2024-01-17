2c9f1a27042bb8ef915cd1d0f736ce6eeab2849d
yj <20211939@edu.hanbat.ac.kr>
2024-01-17 22:13:35 +0900

    javafolder

--- go/go_test/main.go
+++ go/go_test/main.go
@@ -13,6 +13,7 @@ type User struct {
 	Email    string `json:"email"`
 }
 
+// 4
 func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 	// 들어오는 요청의 Response Header에 Content-Type 추가
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -25,22 +26,25 @@ func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 
 func main() {
 
+	// 1
 	mux := http.NewServeMux()
 
+	// 2
 	userHandler := http.HandlerFunc(func(wr http.ResponseWriter, r *http.Request) { // 라우팅 작업, 내가 원하는 경로와 함수 연결
 		switch r.Method {
 		case http.MethodGet: // 조회
-			json.NewEncoder(wr).Encode(users) // 인코딩
+			json.NewEncoder(wr).Encode(users) // 등록된 사용자 json 인코딩하여 응답 보냄
 		case http.MethodPost: // 등록
 			var user User
-			json.NewDecoder(r.Body).Decode(&user) // 디코딩
+			json.NewDecoder(r.Body).Decode(&user) // User구조체로 디코딩
 			users[user.Email] = &user
-			json.NewEncoder(wr).Encode(user) // 인코딩
+			json.NewEncoder(wr).Encode(user) // json 인코딩하여 응답 보냄
 
 		}
 		//wr.Write([]byte("hello"))
 	})
 
+	// 3 /users에 미들웨어와 핸들러 등록
 	mux.Handle("/users", jsonContentTypeMiddleware(userHandler))
-	http.ListenAndServe(":8080", mux) // http패키지 이용하여 서버 띄우기
+	http.ListenAndServe(":8080", mux) // http패키지 이용하여 서버 띄우기(포트 실행)
 }
